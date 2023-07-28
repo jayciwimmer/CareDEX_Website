97a18ba3fa764dfd6a17b3472ed9a098b9724cd6
jayciwimmer <140559107+jayciwimmer@users.noreply.github.com>
2023-07-27 17:45:18 -0700

    update

--- src/pages/ActiveShooterDrill.js
+++ src/pages/ActiveShooterDrill.js
@@ -8,7 +8,7 @@ const ActiveShooterDrill = () => {
             <h1>Welcome to the Active Shooter Drill</h1>
             <p>Hello</p>
             <a href="https://docs.google.com/document/d/1KP6gnt8U1n1AbHriQDuowlHqiub8V-dj7L7jeqTwQCw/edit">
-            <button className="activeshooter">Click me to access the Active Shooter Downloadable Drill Scenario</button>
+            <button className="button">Click me to access the Active Shooter Downloadable Drill Scenario</button>
             
             </a>
             
