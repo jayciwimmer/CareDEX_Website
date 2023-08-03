921f2fad99014b0576506c7acca708ba3152258c
jayciwimmer <140559107+jayciwimmer@users.noreply.github.com>
2023-08-02 17:13:25 -0700

    uploads

--- src/pages/myfacility.js
+++ src/pages/myfacility.js
@@ -4,6 +4,7 @@ const MyFacility = () => {
     return (
         <div>
             <h1>Welcome to the My Facility Page</h1>
+            <h2> On this page, you will be able to upload your residential data to run the drill simulations.</h2>
 
         </div>
     );
