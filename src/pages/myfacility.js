0624ae638ec489c4c9c3a9993161c888cc908eba
jayciwimmer <140559107+jayciwimmer@users.noreply.github.com>
2023-08-03 14:40:41 -0700

    updates

--- src/pages/myfacility.js
+++ src/pages/myfacility.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 
+
 const MyFacility = () => {
     return (
         <div>
             <h1>Welcome to the My Facility Page</h1>
             <h2> On this page, you will be able to upload your residential data to run the drill simulations.</h2>
 
+
+
+        
         </div>
     );
 };
