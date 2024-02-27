6b39aceac47023f0127f501e4ad3210139ae1874
jayciwimmer <140559107+jayciwimmer@users.noreply.github.com>
2024-02-27 17:42:45 -0500

    update firedrill.js with the correct drill

--- src/pages/FireDrill.js
+++ src/pages/FireDrill.js
@@ -6,7 +6,7 @@ const FireDrill = () => {
         <div>
             <h1>Welcome to the Fire Drill</h1>
 
-            <a href="https://docs.google.com/document/d/1Lfv-Otwr1K4gEgCR_fb6G3777AAUPpCSlJ9xnakVCM8/edit">
+            <a href="https://docs.google.com/document/d/1fBrrorqEPvqB7_psrkMr9yjBncuGdeYsD6uVJptq6rM/edit">
             <button className="button">Click me to access the Fire Downloadable Drill Scenario</button>
             </a>
 
